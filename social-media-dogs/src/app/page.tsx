03e0c7e1529d08811849a0d25e1c7698bf4212da
Igor Augusto <igoraugustobz@gmail.com>
2025-07-17 21:07:54 -0300

    :recycle: refactor: adicionar componente a página

--- social-media-dogs/src/app/page.tsx
+++ social-media-dogs/src/app/page.tsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
+import Feed from '@/components/feed/feed';
+import photosGet from '@/actions/photos-get';
 
-export default function Home() {
+export default async function Home() {
+  const data = await photosGet();
+
   return (
     <section className="container mainContainer">
       <h1 className="title">Dogs Next</h1>
+
+      <Feed photos={data} />
     </section>
   );
 }
