8195e941801b13e7ce82d4a474aabf9fcc3c8587
Igor Augusto <igoraugustobz@gmail.com>
2025-07-20 13:12:05 -0300

    :sparkles: feat: adicionar validações de erros na action de login

--- social-media-dogs/src/actions/login.ts
+++ social-media-dogs/src/actions/login.ts
@@ -1,31 +1,45 @@
 'use server';
 
+import { TOKEN_POST } from '@/functions/api';
+import apiError from '@/functions/api-error';
 import { cookies } from 'next/headers';
 
-export default async function login(formData: FormData) {
+export default async function login(state: {}, formData: FormData) {
   const username = formData.get('username') as string | null;
   const password = formData.get('password') as string | null;
 
-  // Obtém o token da API Back-End e valida informações de login
-  const response = await fetch(
-    'https://dogsapi.origamid.dev/json/jwt-auth/v1/token',
-    {
+  try {
+    if (!username || !password) {
+      throw new Error('Preencha os campos de login e senha.');
+    }
+
+    const { url } = TOKEN_POST();
+
+    // Obtém o token da API Back-End e valida informações de login
+    const response = await fetch(url, {
       method: 'POST',
       body: formData,
+    });
+
+    if (!response.ok) {
+      throw new Error('Senha ou usuário inválido.');
     }
-  );
-  const data = await response.json();
 
-  /*
-   * Define um cookie para o token. Desse modo, a responsabilidade
-   * sai do Front-End e passa p/ o navegador
-   */
-  cookies().set('token', data.token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: 'lax',
-    maxAge: 60 * 60 * 24,
-  });
+    const data = await response.json();
 
-  console.log(data);
+    /*
+     * Define um cookie para o token. Desse modo, a responsabilidade
+     * sai do Front-End e passa p/ o navegador
+     */
+    cookies().set('token', data.token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'lax',
+      maxAge: 60 * 60 * 24,
+    });
+
+    return { data: null, ok: true, error: '' };
+  } catch (error: unknown) {
+    return apiError(error);
+  }
 }
