e15951ff430f4aa99af80ba93f10bd23e8054055
Igor Augusto <igoraugustobz@gmail.com>
2025-07-25 11:48:07 -0300

    :sparkles: feat: adicionar metadada

--- social-media-dogs/src/app/login/criar/page.tsx
+++ social-media-dogs/src/app/login/criar/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Crie sua conta',
+  description: 'Crie sua conta no site Dogs.',
+};
+
 export default function CriarPage() {
   return (
     <main>
--- social-media-dogs/src/app/login/perdeu/page.tsx
+++ social-media-dogs/src/app/login/perdeu/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Perdeu a senha | Dogs',
+  description: 'Recupere a sua senha.',
+};
+
 export default function PerdeuPage() {
   return (
     <main>
