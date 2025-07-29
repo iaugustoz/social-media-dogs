27720989050b73770cb5152e7e03c65a2202641e
Igor Augusto <igoraugustobz@gmail.com>
2025-07-29 16:00:36 -0300

    :sparkles: feat: adicionar conteúdo a ContaPage

--- social-media-dogs/src/app/conta/page.tsx
+++ social-media-dogs/src/app/conta/page.tsx
@@ -1,14 +1,42 @@
+import photosGet from '@/actions/photos-get';
+import userGet from '@/actions/user-get';
+import Feed from '@/components/feed/feed';
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metada: Metadata = {
   title: 'Minha Conta',
 };
 
-export default function ContaPage() {
+export default async function ContaPage() {
+  const { data: user } = await userGet();
+  const { data } = await photosGet({ user: user?.username });
+
   return (
     <>
       <main>
-        <h1>Conta</h1>
+        {data?.length ? (
+          <Feed photos={data} />
+        ) : (
+          <div>
+            <p
+              style={{
+                color: '#444',
+                fontSize: '1.25rem',
+                marginBottom: '1rem',
+              }}
+            >
+              Nenhuma foto encontrada.
+            </p>
+            <Link
+              href={'/conta/postar'}
+              className="button"
+              style={{ display: 'inline-block' }}
+            >
+              Postar Foto
+            </Link>
+          </div>
+        )}
       </main>
     </>
   );
