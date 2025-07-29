d00d510a041969cd48e3869730d7cddfee2484cb
Igor Augusto <igoraugustobz@gmail.com>
2025-07-29 15:29:51 -0300

    :recycle: refactor: adicionar config para revalidar o cache da página

--- social-media-dogs/src/actions/photos-get.ts
+++ social-media-dogs/src/actions/photos-get.ts
@@ -13,13 +13,17 @@ export type Photo = {
 };
 
 export default async function photosGet() {
-  
   // Obtém JSON da API Dogs. Esse JSON possui os dados mockados p/ o Front-End
   const response = await fetch(
-    'https://dogsapi.origamid.dev/json/api/photo/?_page=1&_total=6&_user=0'
+    'https://dogsapi.origamid.dev/json/api/photo/?_page=1&_total=6&_user=0',
+    {
+      next: {
+        revalidate: 10,
+        tags: ['photos'],
+      },
+    }
   );
 
   const data = (await response.json()) as Photo[];
-
   return data;
 }
