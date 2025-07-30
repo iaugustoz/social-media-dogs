741f78e3c71f337c400b1104936806f6cdd12adf
Igor Augusto <igoraugustobz@gmail.com>
2025-07-30 11:12:30 -0300

    :recycle: refactor: alterar tipo de retorno e incluir validação

--- social-media-dogs/src/app/page.tsx
+++ social-media-dogs/src/app/page.tsx
@@ -4,13 +4,12 @@ import Feed from '@/components/feed/feed';
 import photosGet from '@/actions/photos-get';
 
 export default async function Home() {
-  const data = await photosGet();
+  const { data } = await photosGet();
 
   return (
     <section className="container mainContainer">
       <h1 className="title">Dogs Next</h1>
-
-      <Feed photos={data} />
+      {data && <Feed photos={data} />}
     </section>
   );
 }
